a39280111b1de7c1c6c8e9023cd64e4ac1d122ab
Allen <allenlovatt@gmail.com>
2021-09-10 00:16:32 -0400

    Site Structure

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -6,3 +6,15 @@ function myFunction() {
       x.className = "topnav";
     }
   }
+
+  const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+  if(!menuOpen) {
+    menuBtn.classList.add('open');
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove('open');
+    menuOpen = false;
+  }
+});
